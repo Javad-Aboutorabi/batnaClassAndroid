1305cd6d4ab8e5e8968325081113f603e6cfdf67
javad <javad.aboutorabi>
2021-07-27 12:50:55 +0430

    Changed activity

--- app/src/main/java/com/javad/testjamshi/MainActivity.java
+++ app/src/main/java/com/javad/testjamshi/MainActivity.java
@@ -1,7 +1,11 @@
 package com.javad.testjamshi;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +36,17 @@ public class MainActivity extends AppCompatActivity {
             Random random = new Random();
             int a = random.nextInt(name.size());
             button.setText(name.get(a).toString());
+            button.setBackgroundColor(Color.argb(255,random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+            button.setTextColor(Color.argb(255,random.nextInt(255-210)+210,random.nextInt(255-210)+210,random.nextInt(255-210)+210));
+            button.setScaleX((float) (random.nextFloat()+0.5));
+            button.setScaleY((float) (random.nextFloat()+0.5));
         });
+        ImageButton button2;
+        button2 = findViewById(R.id.next);
+        button2.setOnClickListener(v -> {
+            Intent intent=new Intent(this,Secound.class);
+            startActivity(intent);
+        });
+
     }
 }
---
+++ app/src/main/java/com/javad/testjamshi/Secound.java
@@ -0,0 +1,14 @@
+package com.javad.testjamshi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Secound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secound);
+    }
+}
