98930223b04276aecc7b3483d1ec50cd76c95382
javad <javad.aboutorabi>
2021-07-31 08:49:20 +0430

    9mordad

--- app/src/main/java/com/javad/testjamshi/Secound.java
+++ app/src/main/java/com/javad/testjamshi/Secound.java
@@ -1,21 +1,45 @@
 package com.javad.testjamshi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Secound extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_secound);
-        View button3 = findViewById(R.id.BackButton);
+        ConstraintLayout constraintLayout=findViewById(R.id.my_layout);
+        Button button3 = findViewById(R.id.BackButton);
+        EditText editText=findViewById(R.id.editText);
         button3.setOnClickListener(v -> {
-            this.finish();
+            button3.setText(editText.getText());
+            constraintLayout.setBackgroundColor(Color.argb(200,201,11,11));
+
         });
+        button3.setOnLongClickListener(v -> {
+            Toast.makeText(this, "finish", Toast.LENGTH_SHORT).show();
+            this.finish();
+            return false;
+        });
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.finish();
     }
 
     @Override
